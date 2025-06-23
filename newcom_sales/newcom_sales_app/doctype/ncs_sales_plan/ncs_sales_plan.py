8f78937e96c7847c31e42d02b6c0d7331bd94d8b
tcb-developer <developertcb01@gmail.com>
2025-06-23 10:39:54 +0530

    customer code generation updated

--- newcom_sales/newcom_sales_app/doctype/ncs_sales_plan/ncs_sales_plan.py
+++ newcom_sales/newcom_sales_app/doctype/ncs_sales_plan/ncs_sales_plan.py
@@ -1,8 +1,23 @@
-# Copyright (c) 2025, TCB and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
+import random, string
+
 
 class NCSSalesPlan(Document):
-	pass
+    pass
+
+
+def generate_customer_code(length=10):
+    chars = string.digits
+    code = "".join(random.choices(chars, k=length))
+    if frappe.db.exists("NCS Sales Plan", code):
+        return generate_customer_code(length=length)
+    else:
+        return code
+
+
+@frappe.whitelist()
+def get_customer_code():
+    length = 10
+    customer_code = generate_customer_code(length=length)
+    return customer_code
